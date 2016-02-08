10fe8e378ef95bafa660a0f7f6e073344f77fc0c
Erik Podetti <erik.podetti@gmail.com>
2016-02-08 21:02:55 +0100

    Haptic graphics

--- client/js/controllers/haptic.js
+++ client/js/controllers/haptic.js
@@ -3,21 +3,20 @@ angular
 	.controller('HapticController', ['$scope', '$rootScope', '$state',
 		function ($scope, $rootScope, $state) {
 
-			$scope.labels = ["0ms", "125ms", "250ms", "375ms", "500ms"];
-			$scope.data = [ [65, 59, 80, 81, 56, 55, 40],
-							[28, 48, 40, 19, 86, 27, 90]  ];
+			$scope.addStep = function() {
 
+				console.log("we added a step " + durationSlider.slider('getValue'));
+				console.log("but the scope is "+ $scope.slider.sliderValue);
+				console.log($scope);
+			};
 
-
-			$('#haptic-slider-vibration-0').slider({
+			var durationSlider = $('#haptic-slider-vibration-0').slider({
 				formatter: function (value) {
 					return value + " ms";
 				},
 				reversed: true,
 				tooltip_position: 'left',
 				tooltip : 'always'
-
-
 			});
 
 			$('#haptic-slider-intensity-0').slider({
@@ -27,8 +26,6 @@ angular
 				reversed: true,
 				tooltip_position: 'left',
 				tooltip : 'always'
-
-
 			});
 
 			$('#haptic-slider-pause-0').slider({
@@ -38,17 +35,11 @@ angular
 				reversed: true,
 				tooltip_position: 'left',
 				tooltip : 'always'
-
-
 			});
 
 
-			//$('#create-haptic-slider').on("slide", function(slideEvt){
-			//	$('#slider-0-value').text(slideEvt.value);
-			//});
-
-
-
-
+			/*$('#haptic-slider-vibration-0').on("slide", function(slideEvt){
+				$scope.slider.sliderValue = slideEvt.value;
+			});*/
 
 		}]);
