fc52a7a4c1612d201b7c24fde034de95b43e7d44
Erik Podetti <erik.podetti@gmail.com>
2016-02-15 12:35:36 +0100

    Fixed edit artwork

--- client/js/controllers/art-work.js
+++ client/js/controllers/art-work.js
@@ -3,22 +3,22 @@ angular
 	.controller('EditArtWorkController', ['$scope', '$rootScope', '$q', '$state', '$stateParams', 'File',
 		function($scope, $rootScope, $q, $state, $stateParams, File){
 
-			$scope.file = {};
+			$scope.artWork = {};
 
 			$q.all([
 				File.findById({ id : $stateParams.id}).$promise
 			])
 				.then(function(data){
 					console.log(data[0]);
-					$scope.file.name = data[0].name;
-					$scope.file.description = data[0].description;
+					$scope.artWork = data[0];
+
 			});
 
 			$scope.submitForm = function()
 			{
-				$scope.file
+				$scope.artWork
 					.$save()
-					.then(function(file){
+					.then(function(artWork){
 						$state.go('area-clienti.opere');
 				});
 			};
